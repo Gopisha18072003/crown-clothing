6709437afbbb1992d6b4b4b9aa718bee9dc3e0ce
Gopisha18072003 <gopishaw111@gmail.com>
2024-01-03 21:12:50 +0530

    started react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Homepage from './pages/homepage/homepage.component'
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/shop/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
